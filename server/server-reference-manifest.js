a8a666c17a7615718c1ad9051a974cf3bd4a30aa
beaux <34432201+beaux-riel@users.noreply.github.com>
2024-08-19 11:50:39 -0700

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"aLjbRIY0+WTv9H/+KGR1PaYX3HvS+Fm1tYsd8PA4eFY=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"eZjhlmTrpr5FiQPr6WA1hYB4vy6fdvItdu8lTEmIhCg=\"}"
