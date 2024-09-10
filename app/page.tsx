6b7d0ac53fb0cd9de20e5c3cc6a9d91195a62548
Beaux W. <beaux.walton@gmail.com>
2024-09-09 17:32:54 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
             type="text"
             value={baseUrl}
             onChange={(e) => setBaseUrl(e.target.value)}
-            placeholder="example.com"
+            placeholder="wildernesscommittee.org"
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black placeholder-gray-500"
             required
           />
